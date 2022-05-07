9a3b4c14f5409f2b629b10f3adfb835e17f39539
William Stockhausen <william.stockhausen@noaa.gov>
2022-05-07 10:03:19 -0700

    correcting pull conflicts.

--- R/figInfo.R
+++ R/figInfo.R
@@ -29,41 +29,4 @@ figInfo<-function(.x,label="",path="",fn="",width=NA_real_,height=NA_real_,orien
   return(y);
 }
 
-<<<<<<< HEAD
 
-=======
-#'
-#' @title Read figInfo from a csv file and add it to an eexisting figInfo object
-#'
-figInfo.ReadCSV<-function(allfi,fncsv){
-  if (is.character(fncsv)) {
-    if (file.exists(fncsv)){
-      dfr = readr::read_csv(fncsv);
-    } else {
-      stop("figInfo.ReadCSV: input csv file '",fncsv,"' not found!")
-    }
-  } else {
-    dfr = fncsv;#--fncsv should be a dataframe
-  }
-
-  for (i in 1:nrow(dfr)){
-    #--testing: i=1;
-    rw = dfr[i,];
-    fn = rw$fn;
-    if (!stringr::str_ends(fn,stringr::regex("([^\\s]+(\\.(?i)(jpe?g|png|gif|bmp|pdf))$)"))) fn = paste0(fn,".pdf");#--see: https://www.geeksforgeeks.org/how-to-validate-image-file-extension-using-regular-expression/
-    if (!file.exists(file.path(rw$path,fn))) {
-      stop("figInfo.ReadCSV: figure file '",file.path(rw$path,fn),"' not found!")
-    }
-    orientation = "portrait"; #--default orientation
-    if (rw$width>6.5) orientation = "landscape";
-    allfi %<>% wtsMarkdown::figInfo(label=rw$label,
-                                    path = rw$path,
-                                    fn   = fn,
-                                    caption=rw$caption,
-                                    width=rw$width,
-                                    height=wtsMarkdown::getAspectRatio(file.path(rw$path,fn))*rw$width,
-                                    orientation=orientation);
-  }
-  return(allfi);
-}
->>>>>>> 2114d8f49972ce60383a86af1fd43943cd47b76c
